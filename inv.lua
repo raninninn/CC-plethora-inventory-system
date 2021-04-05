ec6c90a239c9305a148a99b7a7faae26c9248d12
raninninn <matteshilli@gmail.com>
2021-04-06 01:42:13 +0200

    Fully added feature requested in #1

--- inv.lua
+++ inv.lua
@@ -1,12 +1,18 @@
+-- Configuration
 local chest0 = peripheral.wrap("minecraft:ironchest_iron_0")
 local chest1 = peripheral.wrap("minecraft:ironchest_copper_5")
-local bRunning = true
 
 local tChests = {chest0, chest1}
 
+local inv_dir = "/inventory/"
+
+-- Main code
+
+local bRunning = true
+local defCol = term.getTextColour()
 -- Create working directory, if it doesn't exist
-if fs.isDir("/inventory") == false then
-	fs.makeDir("/inventory")
+if fs.isDir(inv_dir) == false then
+	fs.makeDir(inv_dir)
 end
 
 local function main()
@@ -28,11 +34,12 @@ local function main()
                 if a[i] ~= nil then
                     local id = chest.getItemMeta(i).rawName
                     if tStore[id] ~= nil then
-                        tStore[id]["count"] = chest.getItemMeta(i).count + b[id]
+                        tStore[id]["count"] = chest.getItemMeta(i).count + tStore[id]["count"]
                     else
+						tStore[id] = {}
                         tStore[id]["count"] = chest.getItemMeta(i).count
-						tStore[id]["dispName"] = chest.getItemMetat(i).displayName
-						tStore[id]["chest"] = chest
+						tStore[id]["dispName"] = chest.getItemMeta(i).displayName
+						tStore[id]["chest"] = peripheral.getName(chest)
 						tStore[id]["slot"] = i
                     end
                 
@@ -44,7 +51,9 @@ local function main()
 			-- Make string out of table
 			for key,value in pairs(tStore) do
 				local padding = ""
-				for j=1 , maxb - bid_len do
+				local sCount = tostring( tStore[key]["count"] )
+				local sCountLen = string.len(sCount)
+				for j=1 , maxb - sCountLen do
 					padding = padding .. " "
 				end
 				sOut = sOut..padding..tStore[key]["count"] .."  ".. tStore[key]["dispName"].."\n"
@@ -52,29 +61,30 @@ local function main()
         end
         textutils.pagedPrint(sOut)
 		-- Write stringified table to "item_catalogue"
-        local sFile = fs.open("/inventory/item_catalogue", "w")
+        local sFile = fs.open(inv_dir .. "item_catalogue", "w")
         sFile.write(sOut)
 		sFile.flush()
 		sFile.close()
 		-- Write table to "item_table"
-		local tFile = fs.open("/inventory/item_table", "w")
+		local tFile = fs.open(inv_dir .. "item_table", "w")
 		tFile.write(tStore)
 		tFile.flush()
 		tFile.close()
-    end
-    if input == "quit" then
+    elseif input == "quit" then
         bRunning = false
-    end
-    if input == "ls" then
-        local sFile = fs.open("item_catalogue", "r")
+    elseif input == "ls" then
+        local sFile = fs.open(inv_dir .. "item_catalogue", "r")
         textutils.pagedPrint(sFile.readAll())
-    end
-    if input == "help" then
+    elseif input == "help" then
         print("help: prints this help message")
         print("ls: prints out the contents of the item database")
         print("rebuild: rebuilds the item database")
         print("quit: quits the shell")
-    end
+    else
+		term.setTextColour( colours.red )
+		print("Unknown command")
+		term.setTextColour( defCol )
+	end
 end
 
 repeat main() until bRunning == false
